3013b245f59d4482c99f626462c1cbc2663b1db2
Bn0972 <78458863+Bn0972@users.noreply.github.com>
2025-04-15 14:23:45 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    //Hello and welcome!i = 1 i = 2 i = 3 i = 4 i = 5
+        System.out.print("Hello and welcome!");
+        for(int i = 1; i <=5 ;i++)
+        {
+            System.out.print("i = " + i + " ");
+        }
+    }
+}
