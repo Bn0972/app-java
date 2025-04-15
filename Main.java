d2058f55bfa7cc210e1aa12cc13034b0507b2db6
Bn0972 <78458863+Bn0972@users.noreply.github.com>
2025-04-15 15:43:30 +0200

    Update Main.java222

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         // {
         //     System.out.println("i = " + i + " ");
         // }
-        string[] arr1 = {"Piotr","Anna","Jan","Leszek"};
+        String[] arr1 = {"Piotr","Anna","Jan","Leszek"};
          for(int i = 0 ; i <4 ;i++)
         {
             System.out.println((i+1)+". "+ arr1[i]);
