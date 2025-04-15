6fc818364d8b459f79c5f2b5e71f31fa791328a2
Bn0972 <78458863+Bn0972@users.noreply.github.com>
2025-04-15 15:33:53 +0200

    Update Main.java，change i to 15

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
     //Hello and welcome!i = 1 i = 2 i = 3 i = 4 i = 5
         System.out.println("Hello and welcome!");
-        for(int i = 1; i <=5 ;i++)
+        for(int i = 1; i <=15 ;i++)
         {
             System.out.println("i = " + i + " ");
         }
