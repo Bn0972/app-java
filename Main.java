cf819acc74e18b6e1b19f996bd0dec021a37b1be
Bn0972 <78458863+Bn0972@users.noreply.github.com>
2025-04-15 15:41:27 +0200

    Update Main.java,change output

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
     //Hello and welcome!i = 1 i = 2 i = 3 i = 4 i = 5
-        System.out.println("Hello and welcome!");
-        for(int i = 1; i <=15 ;i++)
+        // System.out.println("Hello and welcome!");
+        // for(int i = 1; i <=15 ;i++)
+        // {
+        //     System.out.println("i = " + i + " ");
+        // }
+        string[] arr1 = {"Piotr","Anna","Jan","Leszek"};
+         for(int i = ; i <4 ;i++)
         {
-            System.out.println("i = " + i + " ");
+            System.out.println((i+1)+". " arr1[i]);
         }
     }
 }
